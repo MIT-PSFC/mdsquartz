64d1b8ceb378500e0b49a79a788634bc89428477
Joshua Stillerman <jas@mit.edu>
2024-01-19 15:51:00 +0000

    recursively add channel groups

--- quartztree.py
+++ quartztree.py
@@ -1,17 +1,29 @@
+def add_items(parent, items):
+    for item in items:
+        print(item['type'])
+        if item['type'] == 'channel_group' :
+            child = parent.addNode(f".{item['name'].upper()}", 'STRUCTURE')
+            add_items(child, item['children'])
+        else:
+            c_node = parent.addNode(item["name"].upper(), 'SIGNAL')
+
 def quartztree(run_id, treename, shotnumber):
     import quartz
     from MDSplus import Tree
-    tree = Tree(treename, -1)
-    tree.createPulse(shotnumber)
-    tree = Tree(treename, shotnumber, 'edit')
-    q_node = tree.QUARTZ
+    tree = Tree(treename, shotnumber, 'new')
+    description_node = tree.addNode('description', 'text')
+    end_time_node = tree.addNode('end_time', 'text')
+    end_time_node.addTag('END_TIME')
+    gid_node = tree.addNode('gid', 'text')
+    name_node = tree.addNode('name', 'text')
+    start_time_node = tree.addNode('start_time', 'text')
+    start_time_node.addTag('START_TIME')
+    zero_time_node = tree.addNode('ZERO_TIME', 'text')
+    zero_time_node.addTag('ZERO_TIME')
+    tags_node = tree.addNode('tags', 'text')
+    parent = tree.addNode('quartz', 'structure')
+
     data_access_client = quartz.DataAccessClient(quartz.PRODUCTION_DATA_ACCESS_URL)
-    chans = data_access_client.get_channels(run_id)
-    for channel_group in chans:
-        g_node = q_node.addNode(f".{channel_group['name'].upper()}", 'STRUCTURE')
-        print(g_node)
-        for channel in channel_group['children']:
-            print(f'  Adding {channel["name"].upper()} to {g_node}')
-            c_node = g_node.addNode(channel["name"].upper(), 'NUMERIC')
-            print(c_node)
+    items = data_access_client.get_channels(run_id)
+    add_items(parent, items)
     tree.write()
